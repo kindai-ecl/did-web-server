e30d8432af59db96849a9b3df6930ea0847db55a
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2024-08-14 18:39:40 +0900

    refactor: more detailed configuration setting at pwa

--- sample/vite.config.js
+++ sample/vite.config.js
@@ -2,12 +2,15 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({
     registerType: 'autoUpdate',
-    injectRegister: false,
+    injectRegister: 'auto',
 
     pwaAssets: {
       disabled: false,
@@ -15,10 +18,12 @@ export default defineConfig({
     },
 
     manifest: {
-      name: 'sample',
-      short_name: 'sample',
-      description: 'issuer sample',
+      name: 'issuer sample app',
+      short_name: 'issuer',
+      description: 'issuer sample app ',
       theme_color: '#7fffbf',
+      display: 'standalone',
+      shart_url: '/'
     },
 
     workbox: {
@@ -28,7 +33,7 @@ export default defineConfig({
     },
 
     devOptions: {
-      enabled: false,
+      enabled: true,
       navigateFallback: 'index.html',
       suppressWarnings: true,
       type: 'module',
@@ -39,11 +44,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src/', import.meta.url)),
     },
   },
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+  },
   server: {
     proxy: {
-      '/v0': 'https://did.lcyou.org',
       '/host':{
-        target: 'https://did.lcyou.org',
+        target: 'https://did.lcyou.org/',
+        // target: JSON.stringify(process.env.VITE_API_URL),
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/host/, ''),
       },
