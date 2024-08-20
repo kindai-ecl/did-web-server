8f27cfc6b80bae8da9fda3b9a5202610f7883eaf
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2024-08-21 01:00:55 +0900

    add: add put document

--- v0/api/document.js
+++ v0/api/document.js
@@ -2,15 +2,21 @@ const diddoc = require("../util/document.js");
 const express = require("express");
 const router = express.Router();
 
-// read document is not necessary
+// did method https://www.w3.org/TR/did-core/#method-operations
+// Read method by going directly to the URL of the DIDDocument
 
-
-
-// send did doc  post->{jwk, controller, id}
+// send did doc  post->{jwk, controller, Auth option}
 router.post("/", (req, res) => {
-    req.body;
     diduri = diddoc.create(req.body);
+    if ( didurl == "" ) {
+        res.status(500).json({"error": "cannot generated"})
+    }
     res.json({ "document-url": diduri });
 }); 
+
+// didcore update method 
+router.put("/", (req, res) => {
+
+});
 
 module.exports = router; 
